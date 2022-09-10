cc2ae67aa4680df288ea0ec50ca235a2f8edca11
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-09-10 08:35:47 +0200

    create register page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,7 +3,18 @@ import img from '../assets/images/not-found.svg';
 import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
-  return <h2>Error page</h2>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not-found" />
+        <h3>Page Not Found</h3>
+        <p>
+          Oops the page you looking for it doesn't exist in this universe...{' '}
+        </p>
+        <Link to="/">Back Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,51 @@
+import { useState, useEffect } from 'react';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
-  return <h2>regsiter page</h2>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    setValues(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={values.name}
+            className="form-input"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
